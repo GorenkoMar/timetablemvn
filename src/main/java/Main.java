de63ea275ab8f0098f6afb88443813202ca40d1d
Gorenko Maria <kirillovsub@yandex.ru>
2024-12-05 08:47:22 +0300

    Reformated code + changes in "TimetableService"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,14 @@ import ru.netology.javaqa.timetablemvn.services.TimetableService;
 
 public class Main {
 
-  public static void main(String[] args) {
-    TimetableService service = new TimetableService();
-    int income = 10_000;
-    int expenses = 3_000;
-    int threshold = 20_000;
+    public static void main(String[] args) {
+        TimetableService service = new TimetableService();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
 
-    int count = service.calc(income, expenses, threshold);
+        int count = service.calc(income, expenses, threshold);
 
-    System.out.println("Можно отдыхать  " + count + "  месяца(ев).");
-  }
+        System.out.println("Можно отдыхать  " + count + "  месяца(ев).");
+    }
 }
--- src/main/java/ru/netology/javaqa/timetablemvn/services/TimetableService.java
+++ src/main/java/ru/netology/javaqa/timetablemvn/services/TimetableService.java
@@ -2,19 +2,19 @@ package ru.netology.javaqa.timetablemvn.services;
 
 public class TimetableService {
 
-  public int calc(int income, int expenses, int threshold) {
+    public int calc(int income, int expenses, int threshold) {
 
-    int money = 0;
-    int count = 0;
-    for (int month = 0; month < 12; month++) {
-      if (money >= threshold) {
-        count++;
-        money = (money - expenses) / 3;
-      } else {
-        money = (money - expenses) + income;
-      }
+        int money = 0;
+        int count = 0;
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) {
+                count++;
+                money = (money - expenses) / 3;
+            } else {
+                money = (money - expenses) + income;
+            }
+        }
+        return count;
     }
-    return count;
-  }
 }
 

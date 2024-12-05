d49a814b08991dcf2e67b978424f046a9e7d2462
Gorenko Maria <kirillovsub@yandex.ru>
2024-12-05 08:35:50 +0300

    V3

--- src/main/java/ru/netology/javaqa/timetablemvn/services/TimetableService.java
+++ src/main/java/ru/netology/javaqa/timetablemvn/services/TimetableService.java
@@ -4,13 +4,12 @@ public class TimetableService {
 
   public int calc(int income, int expenses, int threshold) {
 
-    int money = +income;
+    int money = 0;
     int count = 0;
     for (int month = 0; month < 12; month++) {
       if (money >= threshold) {
         count++;
-        int expensesHoliday = money - ((money - expenses) / 3);
-        money = money - (expensesHoliday + expenses);
+        money = (money - expenses) / 3;
       } else {
         money = (money - expenses) + income;
       }
